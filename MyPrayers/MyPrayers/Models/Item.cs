3cf6e7f7f22305c7f44e1dc4ce897fd8e0b2bbd9
g1357 <36661604+g1357@users.noreply.github.com>
2019-09-02 11:16:32 +0300

    Adds Solution and Project

---
+++ MyPrayers/MyPrayers/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyPrayers.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MyPrayers/MyPrayers/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyPrayers.Models;
+
+namespace MyPrayers.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
